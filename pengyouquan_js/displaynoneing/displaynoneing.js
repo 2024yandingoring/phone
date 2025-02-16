6e1f6fa7215e8869ab23b56157b543ef4940b896
2024yandingoring <2292778595@qq.com>
2025-02-16 21:19:29 +0800

    Delete pengyouquan_js/displaynoneing directory

--- pengyouquan_js/displaynoneing/displaynoneing.js
+++
@@ -1,6 +0,0 @@
-//这里是遍历所有类名为 display_noneing 数据渲染隐藏
-var display_noneing = document.getElementsByClassName("display_noneing");
-
-for (var i = 0; i < display_noneing.length; i++) { // 遍历所有获取到的元素
-    display_noneing[i].style.display = 'none'; // 设置每个元素的 display 样式为 none
-}
